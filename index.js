f192d20123393e75123333e9bbb7eb37cecd8e13
MitchelleTamnai <cheptoektamnai@gmail.com>
2024-04-02 12:54:58 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.body.removeChild(document.querySelector('main#main'))
+
+const newHeader = document.createElement("h1")
+
+newHeader.setAttribute('id', 'victory')
+newHeader.textContent = "Tamnai is the champion"
